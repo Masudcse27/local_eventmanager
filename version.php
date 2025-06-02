53043d42018cc59e10b6ff402cbbb39ee8b711d2
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-06-02 18:38:41 +0600

    1. version file 2.local_eventmanager table create

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_eventmanager';
+$plugin->version = 2025060201;
+$plugin->requires = 2025041400;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
